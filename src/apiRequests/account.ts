5110fb2491cbbe22d638423e8ac671fba03641f0
NTAnh3999 <57097398+NTAnh3999@users.noreply.github.com>
2025-07-20 21:47:01 +0700

    feat: add change password in profile

--- src/apiRequests/account.ts
+++ src/apiRequests/account.ts
@@ -1,6 +1,8 @@
 import http from "@/lib/http";
 import {
   AccountResType,
+  ChangePasswordV2BodyType,
+  ChangePasswordV2ResType,
   UpdateMeBodyType,
 } from "@/schemaValidations/account.schema";
 
@@ -9,6 +11,20 @@ const accountApiRequest = {
   me: () => http.get<AccountResType>(`${prefix}/me`),
   updateMe: (body: UpdateMeBodyType) =>
     http.put<AccountResType>(`${prefix}/me`, body),
+  sChangePasswordV2: (accessToken: string, body: ChangePasswordV2BodyType) =>
+    http.put<ChangePasswordV2ResType>(`${prefix}/change-password-v2`, body, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }),
+  changePasswordV2: (body: ChangePasswordV2BodyType) =>
+    http.put<ChangePasswordV2ResType>(
+      `/api${prefix}/change-password-v2`,
+      body,
+      {
+        baseUrl: "",
+      }
+    ),
 };
 
 export default accountApiRequest;
--- src/app/manage/setting/change-password-form.tsx
+++ src/app/manage/setting/change-password-form.tsx
@@ -11,8 +11,17 @@ import {
 } from "@/schemaValidations/account.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { useChangePasswordMutation } from "@/queries/useAccount";
+import {
+  handleErrorApi,
+  setAccessTokenToLocalStorage,
+  setRefreshTokenToLocalStorage,
+} from "@/lib/utils";
+import { toast } from "@/hooks/use-toast";
 
 export default function ChangePasswordForm() {
+  const changePasswordMutation = useChangePasswordMutation();
+
   const form = useForm<ChangePasswordBodyType>({
     resolver: zodResolver(ChangePasswordBody),
     defaultValues: {
@@ -21,10 +30,31 @@ export default function ChangePasswordForm() {
       confirmPassword: "",
     },
   });
+  const onSubmit = async (data: ChangePasswordBodyType) => {
+    if (changePasswordMutation.isPending) return;
+    try {
+      const result = await changePasswordMutation.mutateAsync(data);
+      setAccessTokenToLocalStorage(result.payload.data.accessToken);
+      setRefreshTokenToLocalStorage(result.payload.data.refreshToken);
+      toast({
+        description: result.payload.message,
+      });
+    } catch (error) {
+      handleErrorApi({
+        error,
+        setError: form.setError,
+      });
+    }
+  };
+  const reset = () => {
+    form.reset();
+  };
 
   return (
     <Form {...form}>
       <form
+        onSubmit={form.handleSubmit(onSubmit, (err) => console.warn(err))}
+        onReset={reset}
         noValidate
         className="grid auto-rows-max items-start gap-4 md:gap-8"
       >
